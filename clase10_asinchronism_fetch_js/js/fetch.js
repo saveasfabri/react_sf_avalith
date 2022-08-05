cea3b94f3a732d6dede14397124224f0fd561e28
Fabricio Hang <saveasfabri@gmail.com>
2022-08-05 09:18:05 -0300

    :sparkles: Add: example code lines

--- clase10_asinchronism_fetch_js/js/fetch.js
+++ clase10_asinchronism_fetch_js/js/fetch.js
@@ -6,11 +6,12 @@ console.log("Fetch");
 // Promise
 
 /* ************************************************************* */
-
 const URL_USERS = "https://jsonplaceholder.typicode.com/users";
 fetch(URL_USERS) // Infiere en que el pedido es un GET
     .then((response) => response.json())
     .then((data) => showData(data)); //Nested Promises
+
+// Con un for
 
 // Usamos como parametro data (trae el json Api)
 function showData(data) {
@@ -23,8 +24,29 @@ function showData(data) {
         `;
     }
     document.getElementById("fetch-petition").innerHTML = body;
-    /* Con document.getElementById("id"), apuntamos al id con el nombre
-    que asignamos en el html y con innerHTML imprimimos lo almacenado,
-    en este caso, en la variable body */
+    //Con document.getElementById("id"), apuntamos al id con el nombre
+    //que asignamos en el html y con innerHTML imprimimos lo almacenado,
+    //en este caso, en la variable body
 };
 
+/* *************************************************************** */
+
+/* Con map */
+
+/* const url_users = "https://jsonplaceholder.typicode.com/users";
+fetch(url_users) // Infiere en que el pedido es un GET
+    .then((response) => response.json())
+    .then((data) => showdata(data)); //Nested promises
+
+// paint the data in the dom with "map" and arrow function format
+const showdata = (data) => {
+    console.log("data", data);
+    let body = ``;
+    data.map((user) => {
+        return (body += `
+                <p> - username: ${user.name}</p>
+            `);
+    });
+    document.getElementById("fetch-petition").innerhtml = body;
+};
+ */
