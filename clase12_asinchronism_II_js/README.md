2311765e18be45e827850927b65115469613864c
Fabricio Hang <saveasfabri@gmail.com>
2022-08-05 16:49:21 -0300

    :wrench: Config files Clase 12

---
+++ clase12_asinchronism_II_js/README.md
@@ -0,0 +1,11 @@
+# SKIL FACTORY - AVALITH
+------------------------------
+## :book:Clase 12 - 05-08
+[No está subida la clase](https://www.youtube.com/watch?v=Qx5Ofj1QMNc&list=PLJPvCr6dK-cmOZSKyBMiQwptaQb30wqHl&index=9)
+
+### Asincronismo II
+
+
+
+__Repositorio :__
+[Ingresar repositorio](https://github.com/saveasfabri/react_sf_avalith/tree/main/clase09_fx_metodos2_js)
---
+++ clase12_asinchronism_II_js/js/asinchronism2.js
@@ -0,0 +1,79 @@
+/* setTimeout(function () {
+    console.log("Hello in 2 seconds");
+}, 2000); 
+//Devuelve: "Hello in 2 seconds"
+// Se muestra en consola a los 2 segundos.
+
+setTimeout(() => {
+    console.log("Hello in 5 seconds");
+}, 5000);
+//Devuelve: "Hello in 5 seconds"
+// Se muestra en consola 5 seg. despues de la fx anterior
+ */
+
+//Ejemplo con console.log()
+
+/* setTimeout(function () {
+  console.log("First console.log()");
+  setTimeout(function () {
+    console.log("Second console.log()");
+    setTimeout(function () {
+      console.log("Third console.log()");
+    }, 3000);
+  }, 1000);
+}, 5000); */
+/* Devuelve:
+    1°: console.log("First console.log()") a los 5 seg.
+    2°: console.log("Second console.log()") 1 seg despues de First
+    3°: console.log("Third console.log()") 3 seg. despues de Second
+*/
+
+// Ejercicio console.log() con3 Arrow Function
+
+/* setTimeout(() => {
+    console.log("First");
+    setTimeout(() => {
+        console.log("Second");
+        setTimeout(() => {
+            console.log("Third");
+        }, 3000);
+    }, 1000);
+}, 5000); */
+//Mismo resultado anterior pero con arrow function
+
+/*  // Exercise Asynchronism
+
+console.log("1");
+setTimeout(function () {
+    console.log("2");
+}, 3000);
+console.log("3");
+setTimeout(function () {
+    console.log("4");
+}, 1000); */
+
+ // Expected output: 1 3 4 2
+
+// Exercise for Call Stack
+/* function installSoftware(software) {
+    console.log("Installing Software " + software);
+}
+
+function customizeEditor(numberExtensions) {
+    if (numberExtensions <= 0) return;
+    console.log(`Installing ${numberExtensions} extensions for VSC`);
+}
+
+function readyForProgramming() {
+    console.log("Environment ready for begin to Programming");
+}
+
+function prepareEnvironment() {
+    installSoftware("Git");
+    installSoftware("Visual Studio Code");
+    installSoftware("Node.js");
+    customizeEditor(5);
+    readyForProgramming();
+}
+prepareEnvironment();
+ */
