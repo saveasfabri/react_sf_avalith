a04b38b3e122c3db1a3e73174caaced3bf49f7dd
Fabricio Hang <saveasfabri@gmail.com>
2022-08-07 22:04:57 -0300

    :sparkles: Add tablet buttons

---
+++ clase12b_TP_1_html_css_js/js/index.js.js
@@ -0,0 +1,10 @@
+
+const play = document.querySelector(".play")
+
+play.addEventListener("click", () => {
+  let etiquetaAudio = document.createElement("audio")
+  etiquetaAudio.setAttribute("src", "./img/RyM.mp3")
+  etiquetaAudio.play()
+  etiquetaAudio.stop()
+});
+
