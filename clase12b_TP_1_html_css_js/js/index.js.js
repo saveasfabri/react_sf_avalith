33d3e621c862a238bb935b1865ac4517843ea48b
Fabricio Hang <saveasfabri@gmail.com>
2022-08-08 20:40:43 -0300

    :sparkles:Add cards

--- clase12b_TP_1_html_css_js/js/index.js.js
+++ clase12b_TP_1_html_css_js/js/index.js.js
@@ -7,3 +7,35 @@ play.addEventListener("click", () => {
   etiquetaAudio.play()
 });
 
+
+const click1 = document.querySelector(".uno")
+
+click1.addEventListener("click", () => {
+  let etiquetaAudio = document.createElement("audio")
+  etiquetaAudio.setAttribute("src", "./img/click.wav")
+  etiquetaAudio.play()
+});
+
+const click2 = document.querySelector(".dos")
+
+click2.addEventListener("click", () => {
+  let etiquetaAudio = document.createElement("audio")
+  etiquetaAudio.setAttribute("src", "./img/click.wav")
+  etiquetaAudio.play()
+});
+
+const click3 = document.querySelector(".tres")
+
+click3.addEventListener("click", () => {
+  let etiquetaAudio = document.createElement("audio")
+  etiquetaAudio.setAttribute("src", "./img/click.wav")
+  etiquetaAudio.play()
+});
+
+const click4 = document.querySelector(".cuatro")
+
+click4.addEventListener("click", () => {
+  let etiquetaAudio = document.createElement("audio")
+  etiquetaAudio.setAttribute("src", "./img/click.wav")
+  etiquetaAudio.play()
+});
