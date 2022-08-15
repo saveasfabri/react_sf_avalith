a97c3b2efcc7cb747a7164e9bf7222a3e5755c7d
Fabricio Hang <saveasfabri@gmail.com>
2022-08-15 10:06:11 -0300

    :sparkles: Add: Clase 11

--- clase12_asinchronism_II_js/README.md
+++ clase12_asinchronism_II_js/README.md
@@ -1,11 +1,11 @@
 # SKIL FACTORY - AVALITH
 ------------------------------
 ## :book:Clase 12 - 05-08
-[No está subida la clase](https://www.youtube.com/watch?v=Qx5Ofj1QMNc&list=PLJPvCr6dK-cmOZSKyBMiQwptaQb30wqHl&index=9)
+[youtube.com/watch?v=Wwy0ASFa0qk&list=PLJPvCr6dK-cmOZSKyBMiQwptaQb30wqHl&index=12](https://www.youtube.com/watch?v=Wwy0ASFa0qk&list=PLJPvCr6dK-cmOZSKyBMiQwptaQb30wqHl&index=12)
 
 ### Asincronismo II
 
 
 
 __Repositorio :__
-[Ingresar repositorio](https://github.com/saveasfabri/react_sf_avalith/tree/main/clase09_fx_metodos2_js)
+[github.com/saveasfabri/react_sf_avalith/tree/main/clase12_asinchronism_II_js](https://github.com/saveasfabri/react_sf_avalith/tree/main/clase12_asinchronism_II_js)
--- clase12_asinchronism_II_js/js/asinchronism2.js
+++ clase12_asinchronism_II_js/js/asinchronism2.js
@@ -1,79 +1,107 @@
-/* setTimeout(function () {
-    console.log("Hello in 2 seconds");
-}, 2000); 
-//Devuelve: "Hello in 2 seconds"
-// Se muestra en consola a los 2 segundos.
+// const URL_GET_POKEMON = "https://pokeapi.co/api/v2/pokemon/25";
 
-setTimeout(() => {
-    console.log("Hello in 5 seconds");
-}, 5000);
-//Devuelve: "Hello in 5 seconds"
-// Se muestra en consola 5 seg. despues de la fx anterior
- */
+// // Exercise for Promise with .then() and fetch()
 
-//Ejemplo con console.log()
+// fetchedPokemon = fetch(URL_GET_POKEMON);
+// fetchedPokemon
+//     .then((response) => response.json()) // Asynchronous operation
+//     .then((pokemon) => showPokemon(pokemon)); // Asynchronous operation
 
-/* setTimeout(function () {
-  console.log("First console.log()");
-  setTimeout(function () {
-    console.log("Second console.log()");
-    setTimeout(function () {
-      console.log("Third console.log()");
-    }, 3000);
-  }, 1000);
-}, 5000); */
-/* Devuelve:
-    1°: console.log("First console.log()") a los 5 seg.
-    2°: console.log("Second console.log()") 1 seg despues de First
-    3°: console.log("Third console.log()") 3 seg. despues de Second
-*/
+// function showPokemon(pokemon) {
+//     console.log("Pokemon", pokemon);
+//     let body = ``;
+//     body += `
 
-// Ejercicio console.log() con3 Arrow Function
+//         <h1>Pokemon rendering with Fetch y Then</h1>
+//         <h2>${pokemon.name}</h2>
+//         <h3>${pokemon.name} pesa: ${pokemon.weight}kg</h3>
+//         <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+//     `;
+//     document.getElementById("pokemon").innerHTML = body;
+// }
 
-/* setTimeout(() => {
-    console.log("First");
-    setTimeout(() => {
-        console.log("Second");
-        setTimeout(() => {
-            console.log("Third");
-        }, 3000);
-    }, 1000);
-}, 5000); */
-//Mismo resultado anterior pero con arrow function
+// // Exercise for Promise with Fetch + Async/Await
+// const fetchedPokemonAsyncAwait = async () => {
+//     const pokemon = await fetch(URL_GET_POKEMON); // 2s
+//     const pokemonJson = await pokemon.json(); // Execution time
+//     showDataWithAsyncAwait(pokemonJson);
 
-/*  // Exercise Asynchronism
+//     console.log("Pokemon in Async Await", pokemonJson);
+// };
+// fetchedPokemonAsyncAwait();
 
-console.log("1");
-setTimeout(function () {
-    console.log("2");
-}, 3000);
-console.log("3");
-setTimeout(function () {
-    console.log("4");
-}, 1000); */
+// const showDataWithAsyncAwait = async (pokemon) => {
+//     console.log("Pokemon in Async Await", pokemon);
+//     let body = ``;
+//     body += `
+//         <h1>Pokemon rendering with Async/Await</h1>
+//         <h2>${pokemon.name}</h2>
+//         <h3>${pokemon.name} pesa: ${pokemon.weight}kg</h3>
+//         <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+//     `;
+//     document.getElementById("pokemon-async-await").innerHTML = body;
+// };
 
- // Expected output: 1 3 4 2
+// const fetchedPokemonWithAxios = axios.get(URL_GET_POKEMON);
+// const responseAPIAxios = fetchedPokemonWithAxios.then((response) => {
+//     console.log("Pokemon in Axios", response.data);
+//     console.log("Response", response);
+//     const { name, weight, sprites } = response.data;
+//     let body = ``;
+//     body += `
+//         <h1>Pokemon rendering with Axios & Then</h1>
+//         <h2>${name}</h2>
+//         <h3>${name} pesa: ${weight}kg</h3>
+//         <img src="${sprites.front_default}" alt="${name}">
+//     `;
+//     document.getElementById("pokemon-axios").innerHTML = body;
+// });
 
-// Exercise for Call Stack
-/* function installSoftware(software) {
-    console.log("Installing Software " + software);
+// Formulario de Contacto
+// Recibir el nombre del usuario, el correo electrónico y un campo para el comentario que quiere enviar
+
+// La página principal debe contener tres botones:
+// Uno que me lleve al formulario
+// Uno que me lleve a la tabla de usuarios
+// Uno que me lleve a las cards.
+
+// Nombre, Id, email, teléfono, ciudad del usuario. nombre de la compañía.
+// Renderizado de la petición a la Rick & Morty API
+
+// // Código que se corta en la tercera línea
+// const onIncorrectFunction = null;
+// console.log("Console.log before execute function");
+// onIncorrectFunction();
+// console.log("Console.log after execute function");
+
+// Código que se corta en la tercera línea
+try {
+    const onIncorrectFunction = null;
+    console.log("Console.log before execute function");
+    onIncorrectFunction();
+    console.log("Console.log after execute function");
+} catch (error) {
+    console.log("Error", error);
+    console.log("Console.log in catch");
+    console.log("Error Name", error.name);
+    console.log("Error Message", error.message);
 }
+console.log("Console.log after try-catch");
 
-function customizeEditor(numberExtensions) {
-    if (numberExtensions <= 0) return;
-    console.log(`Installing ${numberExtensions} extensions for VSC`);
-}
+const getNumberPokemon = prompt("Ingrese el número de pokemon");
+const URL_GET_POKEMON = `https://pokeapi.co/api/v2/pokemon/${getNumberPokemon}`;
 
-function readyForProgramming() {
-    console.log("Environment ready for begin to Programming");
-}
-
-function prepareEnvironment() {
-    installSoftware("Git");
-    installSoftware("Visual Studio Code");
-    installSoftware("Node.js");
-    customizeEditor(5);
-    readyForProgramming();
-}
-prepareEnvironment();
- */
+const getPokemon = async () => {
+    try {
+        if (getNumberPokemon > 25)
+            throw new SyntaxError("El número debe ser menor a 25");
+        const pokemon = await axios.get(URL_GET_POKEMON);
+        console.log("Pokemon", pokemon.data);
+        console.log("Last line in try");
+    } catch (error) {
+        console.log("Error", error);
+        console.log("Error Name", error.name);
+        alert("No existen registros mayores a 25");
+    }
+};
+getPokemon();
